e38803dc7b51a7a40ffb74b123243b54165b0ab4
Matheus Endlich Silveira <matheus.pepele@gmail.com>
2025-04-12 15:27:10 -0300

    Agr vou Começar a fazer a homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Homepage from './Pages/Homepage'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
-    <Main />
-    <Footer />
+    <Homepage/>
   </StrictMode>,
 )
