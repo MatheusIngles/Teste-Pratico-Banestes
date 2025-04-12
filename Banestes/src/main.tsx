364ae52672e50840ec354adec675d9355b813a30
Matheus Endlich Silveira <matheus.pepele@gmail.com>
2025-04-11 22:26:53 -0300

    Tentando Corrigir o Carrossel

--- Banestes/src/main.tsx
+++ Banestes/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Carrossel from './components/Carrossel'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Carrossel />
   </StrictMode>,
 )
